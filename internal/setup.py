7c48e7501f39905a76aaeb573af4e5651c1d8ba3
AndreasHolmAndersen <72497878+AndreasHolmAndersen@users.noreply.github.com>
2024-06-19 14:38:39 +0200

    Update setup.py
    Signed-off-by: AndreasHolmAndersen <72497878+AndreasHolmAndersen@users.noreply.github.com>

--- internal/setup.py
+++ internal/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="common",
-    version="0.5",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
         "aiohttp",
