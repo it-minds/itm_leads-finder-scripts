e79bfedc139b63910195060831353371f4f84fa2
AndreasHolmAndersen <andreas.h.andersen@twoday.com>
2024-06-28 15:03:39 +0200

    changed all funcs to timertriggers

--- functions/function_app.py
+++ functions/function_app.py
@@ -9,75 +9,42 @@ from step_1_linkedin_paginator.main import linkedin_paginator
 
 app = func.FunctionApp()
 
-connection_str = os.environ.get("AzureWebJobsServiceBus")
-step_1_trigger_name = os.environ.get("ServiceBusStep1Trigger")
-step_2_trigger_name = os.environ.get("ServiceBusStep2Trigger")
-step_3_trigger_name = os.environ.get("ServiceBusStep3Trigger")
-
-
-# # # # # Step 1
-# # Jobindex paginator (Timer trigger)
+# Step 1 - Jobindex paginator (Timer trigger)
 @app.function_name("JobindexPaginatorTimerTrigger")
 @app.timer_trigger(schedule="0 0 * * *", arg_name="myTimer", run_on_startup=False)
 def jobindex_paginator_timer_trigger(myTimer: func.TimerRequest) -> None:
     try:
         jobindex_paginator()
     except Exception as e:
-        logging.warning(f"jobindex paginator failed: str {e}") 
-    # Send a message to the Service Bus
-    with ServiceBusClient.from_connection_string(connection_str) as client:
-        sender = client.get_queue_sender(queue_name=step_1_trigger_name)
-        with sender:
-            message = ServiceBusMessage("jobindex Paginator completed")
-            sender.send_messages(message)
+        logging.warning(f"Jobindex paginator failed: {e}")
 
-# # Linkedin paginator (Servicebus queue trigger)
-@app.function_name("LinkedinPaginatorServiceBusQueueTrigger")
-@app.service_bus_queue_trigger(arg_name="msg", connection="AzureWebJobsServiceBus", queue_name=step_1_trigger_name)
-def linkedin_paginator_servicebus_queue_trigger(msg: func.ServiceBusMessage):
+# Step 2 - Linkedin paginator (Timer trigger)
+@app.function_name("LinkedinPaginatorTimerTrigger")
+@app.timer_trigger(schedule="06 0 * * *", arg_name="myTimer", run_on_startup=False)
+def linkedin_paginator_timer_trigger(myTimer: func.TimerRequest) -> None:
     try:
         linkedin_paginator()
     except Exception as e:
-        logging.warning(f"linkedind paginator failed: str {e}") 
-    # Send a message to the Service Bus
-    with ServiceBusClient.from_connection_string(connection_str) as client:
-        sender = client.get_queue_sender(queue_name=step_2_trigger_name)
-        with sender:
-            message = ServiceBusMessage("Linkedin Paginator completed")
-            sender.send_messages(message)
+        logging.warning(f"Linkedin paginator failed: {e}")
 
-# # # # # Step 2
-# # Link Scraper (Servicebus queue trigger)
-@app.function_name("LinkScraperServiceBusQueueTrigger")
-@app.service_bus_queue_trigger(arg_name="msg", connection="AzureWebJobsServiceBus", queue_name=step_2_trigger_name)
-def link_scraper_servicebus_queue_trigger(msg: func.ServiceBusMessage):
+# Step 3 - Link Scraper (Timer trigger)
+@app.function_name("LinkScraperTimerTrigger")
+@app.timer_trigger(schedule="12 0 * * *", arg_name="myTimer", run_on_startup=False)
+def link_scraper_timer_trigger(myTimer: func.TimerRequest) -> None:
     try:
         link_scraper()
     except Exception as e:
-        logging.warning(f"linkedind paginator failed: str {e}") 
-    # Send a message to the Service Bus
-    with ServiceBusClient.from_connection_string(connection_str) as client:
-        sender = client.get_queue_sender(queue_name=step_3_trigger_name)
-        with sender:
-            message = ServiceBusMessage("Link Scraper completed")
-            sender.send_messages(message)
+        logging.warning(f"Link scraper failed: {e}")
 
-# # # # # Step 3
-# # Text to Model (Servicebus queue trigger)
-@app.function_name("TextToModelServiceBusQueueTrigger")
-@app.service_bus_queue_trigger(arg_name="msg", connection="AzureWebJobsServiceBus", queue_name=step_3_trigger_name)
-def text_to_model_servicebus_queue_trigger(msg: func.ServiceBusMessage):
+# Step 4 - Text to Model (Timer trigger)
+@app.function_name("TextToModelTimerTrigger")
+@app.timer_trigger(schedule="18 0 * * *", arg_name="myTimer", run_on_startup=False)
+def text_to_model_timer_trigger(myTimer: func.TimerRequest) -> None:
     try:
         items = get_cosmos_items()
         if items:
-            logging.warning(f"{len(items)} entries found - proccessing 10")
+            logging.warning(f"{len(items)} entries found - processing 10")
             text_to_model_func(items)
-            # Send a message to the Service Bus
-            # with ServiceBusClient.from_connection_string(connection_str) as client:
-            #     sender = client.get_queue_sender(queue_name=step_3_trigger_name)
-            #     with sender:
-            #         message = ServiceBusMessage("Text to model completed")
-            #         sender.send_messages(message)
-                    
     except Exception as e:
-        logging.warning(f"text_to_model failed: str {e}") 
+        logging.warning(f"Text to model failed: {e}")
+
